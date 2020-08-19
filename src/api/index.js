48330e498eb3f9760b786fcb85491c8760850199
Priyanga Gamage <pgamag3@gmail.com>
2020-08-19 23:57:44 +0100

    getting and setting data from json files

---
+++ src/api/index.js
@@ -0,0 +1,6 @@
+import aircrafts from "./aircrafts.json";
+import flights from "./flights.json";
+
+export const getAircrafts = () => aircrafts;
+
+export const getFlights = () => flights;
--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { getFlights, getAircrafts } from "../api";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [aircrafts, setAircrafts] = useState([]);
+  const [flights, setFlights] = useState([]);
+
+  useEffect(() => {
+    setAircrafts(getAircrafts());
+    setFlights(getFlights());
+  }, []);
+
+  return <div className="App"></div>;
 }
 
 export default App;
