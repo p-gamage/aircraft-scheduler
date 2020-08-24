e2bdb917dac4e2b3082f1cfaaf392f88c84c4785
Priyanga Gamage <pgamag3@gmail.com>
2020-08-24 18:57:30 +0200

    Updates

---
+++ precache-manifest.3c83a92d0b12277cd061df8f204212fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de53e0ba62dbe00db13e3f4899fb44a1",
+    "url": "/aircraft-scheduler/index.html"
+  },
+  {
+    "revision": "84e6b20038a6696f3a3c",
+    "url": "/aircraft-scheduler/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5d4d10c24ee1c557d73f",
+    "url": "/aircraft-scheduler/static/js/2.5458680f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/aircraft-scheduler/static/js/2.5458680f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84e6b20038a6696f3a3c",
+    "url": "/aircraft-scheduler/static/js/main.a5a60f42.chunk.js"
+  },
+  {
+    "revision": "857c24be9c07961ccbac",
+    "url": "/aircraft-scheduler/static/js/runtime-main.91f84162.js"
+  }
+]);
